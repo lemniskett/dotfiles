5a0f68d512f82b9a0995f83b243d581702f6d882
Syahrial Agni Prasetya <lemniskett@outlook.com>
2021-04-16 15:56:59 +0700

    Fri Apr 16 03:56:59 PM WIB 2021

--- desktop/sway/autostart.sh
+++ desktop/sway/autostart.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 #/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh
+flatpak run --branch=stable --arch=x86_64 --command=megasync nz.mega.MEGAsync &
+flatpak run --command=telegram-desktop org.telegram.desktop -workdir /home/lemniskett/.var/app/org.telegram.desktop/data/TelegramDesktop/ -autostart 
 ~/.config/sway/scripts/turn_off_screen.sh &
 swaybg -i ~/.config/sway/images/wallpaper.png &
 xhost si:localuser:root &
@@ -9,7 +11,5 @@ foot --server &
 sleep 2
 mako &
 ~/.config/sway/scripts/battery_notify.sh &
-flatpak run --command=telegram-desktop org.telegram.desktop -workdir /home/lemniskett/.var/app/org.telegram.desktop/data/TelegramDesktop/ -autostart &
-flatpak run --branch=stable --arch=x86_64 --command=megasync nz.mega.MEGAsync &
 # Auto starts after 15 seconds upon login
 sleep 13
