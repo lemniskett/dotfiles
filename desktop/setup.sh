07c8baa5e22c95c8c90ad56338bf6ea0594a4b74
lemniskett <lemniskett@outlook.com>
2021-04-20 09:50:22 +0700

    New colorscheme.

--- desktop/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-for i in */; do rm -rf ~/.config/$i ; ln -sf $(pwd)/$i ~/.config ; done
--- desktop/sway/autostart.sh
+++ desktop/sway/autostart.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-#/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh
-flatpak run --branch=stable --arch=x86_64 --command=megasync nz.mega.MEGAsync &
-flatpak run --command=telegram-desktop org.telegram.desktop -workdir /home/lemniskett/.var/app/org.telegram.desktop/data/TelegramDesktop/ -autostart 
+waybar &
 ~/.config/sway/scripts/turn_off_screen.sh &
 swaybg -i ~/.config/sway/images/wallpaper.png &
 xhost si:localuser:root &
 foot --server &
-# Auto starts after 2 seconds upon login
-sleep 2
 mako &
 ~/.config/sway/scripts/battery_notify.sh &
-# Auto starts after 15 seconds upon login
-sleep 13
+telegram-desktop -startintray &
+sleep 30
+megasync &
+
