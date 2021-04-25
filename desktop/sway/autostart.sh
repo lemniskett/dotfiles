6c1082b4eee1ae0dae3d4e7f1b592c0d70c1f931
Syahrial Agni Prasetya <lemniskett@outlook.com>
2021-04-25 15:35:37 +0700

    New rice

--- desktop/sway/autostart.sh
+++ desktop/sway/autostart.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-waybar &
 ~/.config/sway/scripts/turn_off_screen.sh &
 swaybg -i ~/.config/sway/images/wallpaper.png &
 xhost si:localuser:root &
 foot --server &
 mako &
 ~/.config/sway/scripts/battery_notify.sh &
+waybar -c ~/.config/waybar/config-left.json -s ~/.config/waybar/style-left.css &
+waybar -c ~/.config/waybar/config-top.json -s ~/.config/waybar/style-top.css &
 telegram-desktop -startintray &
-sleep 30
+sleep 60
 megasync &
 
