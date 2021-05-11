e5c67cf126548a3e2282f44a0ee5656fbdf4fade
Syahrial Agni Prasetya <lemniskett@protonmail.com>
2021-05-11 20:52:49 +0700

    Tue May 11 08:52:49 PM WIB 2021

--- desktop/sway/autostart.sh
+++ desktop/sway/autostart.sh
@@ -2,6 +2,7 @@
 
 ~/.config/sway/scripts/turn_off_screen.sh &
 swaybg -i ~/.config/sway/images/wallpaper.png &
+#~/.config/sway/scripts/fade.py & 
 xhost si:localuser:root &
 foot --server &
 mako &
@@ -10,6 +11,7 @@ waybar -c ~/.config/waybar/config-left.json -s ~/.config/waybar/style-left.css &
 telegram-desktop -startintray &
 sleep 1
 waybar -c ~/.config/waybar/config-top.json -s ~/.config/waybar/style-top.css &
+lxpolkit &
 sleep 45
 env QT_QPA_PLATFORM="" megasync &
 
