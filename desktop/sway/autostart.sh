ab3c57b389041752da03ce2b862f8dc0217134f8
Syahrial Agni Prasetya <lemniskett@protonmail.com>
2021-05-13 15:48:39 +0700

    Some changes

--- desktop/sway/autostart.sh
+++ desktop/sway/autostart.sh
@@ -2,14 +2,13 @@
 
 ~/.config/sway/scripts/turn_off_screen.sh &
 swaybg -i ~/.config/sway/images/wallpaper.png &
-#~/.config/sway/scripts/fade.py & 
 xhost si:localuser:root &
 foot --server &
 mako &
 ~/.config/sway/scripts/battery_notify.sh &
 waybar -c ~/.config/waybar/config-left.json -s ~/.config/waybar/style-left.css &
 telegram-desktop -startintray &
-sleep 1
+sleep 0.1
 waybar -c ~/.config/waybar/config-top.json -s ~/.config/waybar/style-top.css &
 lxpolkit &
 sleep 45
