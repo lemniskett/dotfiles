f0aafa3aeffa1e975eabbaecdb52e9ae85f47df4
Syahrial Agni Prasetya <lemniskett@protonmail.com>
2021-07-01 21:36:56 +0700

    Use fzf as launcherThu Jul  1 09:36:56 PM WIB 2021

--- desktop/sway/scripts/launcher.sh
+++ desktop/sway/scripts/launcher.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-PID=$(pidof nwggrid) && kill $PID || nwggrid -b 2e3440ff #-d '/home/lemniskett/.local/share/applications:/home/lemniskett/.local/share/applications/archbox'
+#footclient --app-id=launcher sh -c 'compgen -c | sort -u | fzf --no-info --color="bg:#2e3440,fg:#d8dee9,bg+:#81a1c1,fg+:#2e3440,pointer:#81a1c1,hl+:#2e3440,hl:#81a1c1" | xargs -r swaymsg -t command exec'
+j4-dmenu-desktop --no-exec --dmenu="fzf --color=\"bg:#2e3440,fg:#d8dee9,bg+:#81a1c1,fg+:#2e3440,pointer:#81a1c1,hl+:#2e3440,hl:#81a1c1\"" | xargs -r swaymsg -t command exec
--- desktop/sway/scripts/powermenu.sh
+++ desktop/sway/scripts/powermenu.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-nwgbar -b 2e3440ff
+opt=$(echo -e "Lock Screen\nLog Out\nReboot\nShutdown" | fzf --color="bg:#2e3440,fg:#d8dee9,bg+:#81a1c1,fg+:#2e3440,pointer:#81a1c1,hl+:#2e3440,hl:#81a1c1")
+
+echo $opt
+
+case $opt in
+    "Lock Screen")
+        ~/.config/sway/scripts/lockscreen-blur.sh
+    ;;
+    "Log Out")
+        swaymsg exit
+    ;;
+    "Reboot")
+        reboot
+    ;;
+    "Shutdown")
+        poweroff
+    ;;
+esac
