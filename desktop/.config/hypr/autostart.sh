52e9046830942fe22378c6c739334573460b02e4
Syahrial Agni Prasetya <syahrial@lemniskett.moe>
2024-12-18 11:52:17 +0700

    Stuff

--- desktop/.config/hypr/autostart.sh
+++ desktop/.config/hypr/autostart.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source ~/.angelrc || true
+
 s() {
     systemctl --user start $1
 }
@@ -10,4 +12,9 @@ s mako
 s lxpolkit
 s waybar@left
 s wob
+sleep 1
 s nwg-drawer
+
+# Necessary since Steam Deck Mode sets the cursor-theme to its own
+gsettings set org.gnome.desktop.interface cursor-size "$HYPRDOSE_CURSOR_SIZE"
+gsettings set org.gnome.desktop.interface cursor-theme "$HYPRDOSE_CURSOR_THEME"
